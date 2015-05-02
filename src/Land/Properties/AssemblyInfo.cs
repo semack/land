f771d6a4f3069137572857c7e4c070b7aa5bada1
Andriy S'omak <semack@gmail.com>
2015-05-02 07:48:14 +0300

    Completely converted to MonoGame
    - Build WIN using VS 2013
    - Build OSX using Xamarin Studio

---
+++ src/Land/Properties/AssemblyInfo.cs
@@ -0,0 +1,30 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+
+#if __ANDROID__
+using Android.App;
+#endif
+
+// Information about this assembly is defined by the following attributes.
+// Change them to the values specific to your project.
+
+[assembly: AssemblyTitle("LAND")]
+[assembly: AssemblyProduct("LAND game")]
+[assembly: AssemblyDescription("Ported using MonoGame framework by Andriy S'omak (semack@gmail.com), April 2014")]
+[assembly: AssemblyCompany("ASP corp.")]
+[assembly: AssemblyCopyright("© 1986 ASP corp.")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
+// The form "{Major}.{Minor}.*" will automatically update the build and revision,
+// and "{Major}.{Minor}.{Build}.*" will update just the revision.
+
+[assembly: AssemblyVersion ("1.0.0")]
+
+// The following attributes are used to specify the signing key for the assembly,
+// if desired. See the Mono documentation for more information about signing.
+
+//[assembly: AssemblyDelaySign(false)]
+//[assembly: AssemblyKeyFile("")]
+
